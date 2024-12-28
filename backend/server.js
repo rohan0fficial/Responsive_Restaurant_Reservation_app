67f128eb8d653634a6770fa0db97b43d46ebf811
rohanofficial <rohanraj995590@gmail.com>
2024-12-28 21:47:58 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import app from "./app.js";
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
+
+})
+
+
+
+
+
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+            Welcome to my Restaurant, where culinary excellence meets a warm and inviting atmosphere. Nestled in the heart of sweetness, our restaurant is dedicated to providing a memorable dining experience.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
